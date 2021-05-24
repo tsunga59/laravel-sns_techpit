053aefed5a1b78af6dbe46266ba12579f7229ad2
tsunga59 <tatsumochi@Tatsu-MacBookPro.local>
2021-05-24 10:34:53 +0900

    ユーザー登録・ログアウトを作成

--- laravel/resources/views/nav.blade.php
+++ laravel/resources/views/nav.blade.php
@@ -3,17 +3,23 @@
     <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>memo</a>
   
     <ul class="navbar-nav ml-auto">
-  
+
+      @guest
       <li class="nav-item">
-        <a class="nav-link" href="">ユーザー登録</a>
+        <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="">ログイン</a>
       </li>
+      @endguest
+
+      @auth
       <li class="nav-item">
         <a class="nav-link" href=""><i class="fas fa-pen mr-1"></i>投稿する</a>
       </li>
-  
+      @endauth
+
+      @auth
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
            aria-haspopup="true" aria-expanded="false">
@@ -29,8 +35,9 @@
           </button>
         </div>
       </li>
-      <form id="logout-button" method="POST" action="">
-
+      @endauth
+      <form id="logout-button" method="POST" action="{{ route('logout') }}">
+        @csrf
       </form>
     </ul>
   
