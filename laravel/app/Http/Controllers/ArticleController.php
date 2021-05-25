fc5ce9db6cbd056ab707d1e8205a4d24c0e75481
tsunga59 <tatsumochi@Tatsu-MacBookPro.local>
2021-05-25 10:13:44 +0900

    記事更新・削除・詳細を作成

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Article::class, 'article');
+    }
+    
     public function index()
     {
         $articles = Article::all()->sortByDesc('created_at');
@@ -23,33 +28,34 @@ class ArticleController extends Controller
     public function store(ArticleRequest $request, Article $article)
     {
         $article->user_id = $request->user()->id;
-        // $article->title = $request->title;
-        // $article->body = $request->body;
         $article->fill($request->all());
-
         $article->save();
 
         return redirect()->route('articles.index');
     }
 
-    public function show($id)
+    public function show(Article $article)
     {
-
+        return view('articles.show', ['article' => $article]);
+    }
+    
+    public function edit(Article $article)
+    {
+        return view('articles.edit', ['article' => $article]);
     }
 
-    public function edit($id)
+    public function update(ArticleRequest $request, Article $article)
     {
+        $article->fill($request->all())->save();
 
+        return redirect()->route('articles.index');
     }
 
-    public function update(ArticleRequest $request)
+    public function destroy(Article $article)
     {
-        
-    }
+        $article->delete();
 
-    public function destroy($id)
-    {
-
+        return redirect()->route('articles.index');
     }
     
 }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -15,3 +15,4 @@ Auth::routes();
 
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
+Route::resource('/articles', 'ArticleController')->only(['show']);
